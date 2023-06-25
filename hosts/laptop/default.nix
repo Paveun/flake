3c2386b3f3eb7ea62ae0b53d10894792df307a24
paveun <a.tresierrajansen@gmail.com>
2023-06-25 04:44:25 -0400

    cleanup nvidia setup

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, user, ... }:
+{ config, pkgs, user, lib, ... }:
 
 {
   imports =
@@ -20,30 +20,40 @@
   boot.kernelParams = [
    "tuxedo_keyboard.mode=0"
    "tuxedo_keyboard.brightness=255"
-   "tuxedo_keyboard.color_left=0xffffff"
+   #"tuxedo_keyboard.color_left=0xffffff"
   ];
 
   networking.hostName = "intl"; # Define your hostname.
 
+  # NVIDIA drivers are unfree.
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "nvidia-x11"
+  ];
+
   # Configuring Nvidia PRIME
-  hardware.nvidia.nvidiaSettings = true;
   services.xserver.videoDrivers = ["nvidia"];
-  hardware.opengl.enable = true;
-  hardware.nvidia.modesetting.enable = true;
-  hardware.nvidia.prime = {
-    offload.enable = true;
-    # sync.enable = true; # Keeps GPU always on
-    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-    nvidiaBusId = "PCI:1:0:0";
 
-    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-    intelBusId = "PCI:0:2:0";
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
   };
-  hardware.opengl.driSupport32Bit = true;
-  hardware.nvidia.powerManagement.enable = true;
-  
-  # Optionally, you may need to select the appropriate driver version for your specific GPU.
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
 
+  hardware.nvidia = {
+    modesetting.enable = true;
+    nvidiaSettings = true;
+    powerManagement.enable = true;
+    prime = {
+      offload.enable = true;
+      # sync.enable = true; # Keeps GPU always on
+      # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+      nvidiaBusId = "PCI:1:0:0";
 
+      # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+      intelBusId = "PCI:0:2:0";
+    };
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 }
