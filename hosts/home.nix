04f78015c987e091e0bfb2ce129a60d38c663721
paveun <a.tresierrajansen@gmail.com>
2023-06-27 02:16:37 -0400

    switched from firefox to brave

--- hosts/home.nix
+++ hosts/home.nix
@@ -18,7 +18,8 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    firefox
+    #firefox
+    brave
     kate
     vscode
     google-chrome
