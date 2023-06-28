05e3c40ed2f95e94c6f5483d94008407e515b642
Paveun <a.tresierrajansen@gmail.com>
2023-06-28 16:55:26 -0400

    added razergenie

--- hosts/fishtank/home.nix
+++ hosts/fishtank/home.nix
@@ -21,6 +21,7 @@
     protonvpn-gui
     conda
     liquidctl
+    razergenie
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
