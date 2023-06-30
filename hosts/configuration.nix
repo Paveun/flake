3dd66047ef05e4ce678bb185571dc9a77339e32d
paveun <a.tresierrajansen@gmail.com>
2023-06-30 04:18:31 -0400

    fix smb mount

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -110,6 +110,7 @@
       nitch
       btop
       python311
+      cifs-utils
     ];
   };
 
@@ -132,14 +133,12 @@
     config.credential.helper = "libsecret";
   };
 
-  environment.systemPackages = [ pkgs.cifs-utils ];
   fileSystems."/mnt/qnas" = {
       device = "//10.1.0.8/qnas";
       fsType = "cifs";
       options = let
         # this line prevents hanging on network split
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-
       in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
   };
 
