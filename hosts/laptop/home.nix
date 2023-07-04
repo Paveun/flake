05a1cd969b31c0725c752ef0c9498ecf5aec4d7e
Paveun <a.tresierrajansen@gmail.com>
2023-07-04 12:03:26 -0400

    move config loads for hyprland to folder

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -39,34 +39,4 @@
     #   echo "Hello, ${config.home.username}!"
     # '')
   ];
-  home.file = {
-    ".config/hypr/hyprland.conf"={
-      source = ../../config/hyprland.conf;
-    };
-    ".config/hypr/hyprpaper.conf"={
-      source = ../../config/hyprpaper.conf;
-    };
-    ".config/waybar/config"={
-      source = ../../config/waybar/config;
-    };
-    ".config/waybar/style.css"={
-      source = ../../config/waybar/style.css;
-    };
-    ".config/wallpapers/mountain.jpg"={
-      source = ../../resources/mountain.jpg;
-    };
-    ".config/wallpapers/mountain2.jpg"={
-      source = ../../resources/mountain2.jpg;
-    };
-  };
-  xdg.desktopEntries = {
-    chrome = {
-      comment = "Google Chrome web browser";
-      exec = "google-chrome-stable --enable-accelerated-video-decode --enable-features=UseOzonePlatform --ozone-platform=wayland";
-      genericName = "Google Chrome";
-      icon = "google-chrome-stable";
-      name = "Google Chrome";
-      type = "Application";
-    };
-  };
 }
