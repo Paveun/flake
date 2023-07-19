2e09d31de053469faa9f59082650357ad9dafb6c
paveun <a.tresierrajansen@gmail.com>
2023-07-19 10:27:31 -0400

    update webcord desktop entry

--- home/hosts/fishtank/home.nix
+++ home/hosts/fishtank/home.nix
@@ -31,5 +31,13 @@
       name = "Postman";
       type = "Application";
     };
+    webcord = {
+      categories = [ "Network" "InstantMessaging" ];
+      comment = "A Discord and Fosscord electron-based client implemented without Discord API";
+      exec = "webcord --disable-gpu";
+      icon = "webcord";
+      name = "WebCord";
+      type = "Application";
+    };
   };
 }
