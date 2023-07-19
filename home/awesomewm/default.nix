4772ec5d2f7f4231e2e7267175e9c8f08abfb651
paveun <a.tresierrajansen@gmail.com>
2023-07-19 08:54:37 -0400

    add awesomewm

---
+++ home/awesomewm/default.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, user, lib, ... }:
+
+{
+  services.xserver = {
+    enable = true;
+
+    windowManager.awesome = {
+      enable = true;
+      luaModules = with pkgs.luaPackages; [
+        luarocks # is the package manager for Lua modules
+        luadbi-mysql # Database abstraction layer
+      ];
+    };
+  };
+}
--- home/hosts/laptop/default.nix
+++ home/hosts/laptop/default.nix
@@ -8,6 +8,7 @@
     # ../../gnome
     # ../../plasma
     ../../hyprland
+    ../../awesomewm
   ];
 
   boot = {
