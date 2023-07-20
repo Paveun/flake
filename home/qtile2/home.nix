829add66ed5fbeef869e0685a26167c61fa88bec
paveun <a.tresierrajansen@gmail.com>
2023-07-20 09:24:18 -0400

    add gtk theme to qtile

--- home/qtile2/home.nix
+++ home/qtile2/home.nix
@@ -1,7 +1,29 @@
 { config, pkgs, lib, user, ... }:
 
 {
- home.file = {
+  gtk = {
+    enable = true;
+    font.name = "Roboto";
+    theme = {
+      name = "Juno";
+      package = pkgs.juno-theme;
+    };
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+  };
+  home.file = {
     ".config/qtile/config.py"={
       source = ./config.py;
     };
