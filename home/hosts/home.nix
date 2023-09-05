3b931f7d12e4d4f9cf16a2d43cf154bd2a829835
paveun <a.tresierrajansen@gmail.com>
2023-09-05 10:14:00 -0400

    updates + install jq

--- home/hosts/home.nix
+++ home/hosts/home.nix
@@ -48,6 +48,7 @@
     distrobox
     openconnect
     flameshot
+    jq
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
