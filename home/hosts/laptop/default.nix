6be58ac8a401370e0b8fbc17be5198fd54cf076c
paveun <a.tresierrajansen@gmail.com>
2024-04-02 17:05:28 -0400

    remove kernel params

--- home/hosts/laptop/default.nix
+++ home/hosts/laptop/default.nix
@@ -14,9 +14,9 @@
   boot = {
     kernelParams =
       [
-        "acpi_rev_override"
-        "mem_sleep_default=deep"
-        "intel_iommu=igfx_off"
+        # "acpi_rev_override"
+        # "mem_sleep_default=deep"
+        # "intel_iommu=igfx_off"
       ];
     kernelPackages = pkgs.linuxPackages_latest;
     # extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
@@ -30,8 +30,6 @@
   };
 
   hardware.tuxedo-keyboard.enable = true;
-
-  hardware.acpilight.enable = true;
 
   users.users.${user}.extraGroups = [
     "input"
