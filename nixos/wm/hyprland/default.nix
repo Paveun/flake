ac4cb64c1ffbcb26201016c68100d85ee721bac5
paveun <a.tresierrajansen@gmail.com>
2024-04-13 03:00:03 -0400

    update syntax

--- nixos/wm/hyprland/default.nix
+++ nixos/wm/hyprland/default.nix
@@ -14,18 +14,18 @@
         layout = "us";
         variant = "";
       };
-      displayManager = {
-        sddm = {
-          enable = true;
-          theme = "${(pkgs.fetchFromGitHub {
-            owner = "rototrash";
-            repo = "tokyo-night-sddm";
-            rev = "320c8e74ade1e94f640708eee0b9a75a395697c6";
-            sha256 = "JRVVzyefqR2L3UrEK2iWyhUKfPMUNUnfRZmwdz05wL0=";
-          })}";
-        };
-        defaultSession = "hyprland";
+    };
+    displayManager = {
+      sddm = {
+        enable = true;
+        theme = "${(pkgs.fetchFromGitHub {
+          owner = "rototrash";
+          repo = "tokyo-night-sddm";
+          rev = "320c8e74ade1e94f640708eee0b9a75a395697c6";
+          sha256 = "JRVVzyefqR2L3UrEK2iWyhUKfPMUNUnfRZmwdz05wL0=";
+        })}";
       };
+      defaultSession = "hyprland";
     };
   };
 
