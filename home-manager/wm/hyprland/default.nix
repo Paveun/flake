44daa26b3bb3e0abac2b15b4424a9a9299bcf455
paveun <a.tresierrajansen@gmail.com>
2024-04-14 03:28:19 -0400

    setup hyprlock

--- home-manager/wm/hyprland/default.nix
+++ home-manager/wm/hyprland/default.nix
@@ -7,15 +7,41 @@
     ./hyprland.nix
   ];
 
-  home.file = {
-    # ".config/hypr/hyprland.conf"={
-    #   source = ./hypr/hyprland.conf;
-    # };
-    ".config/hypr/hyprpaper.conf"={
-      source = ./hypr/hyprpaper.conf;
+  home = {
+    file = {
+      # ".config/hypr/hyprland.conf"={
+      #   source = ./hypr/hyprland.conf;
+      # };
+      ".config/hypr/hyprpaper.conf"={
+        source = ./hypr/hyprpaper.conf;
+      };
+      ".config/hypr/hyprlock.conf"={
+        source = ./hypr/hyprlock.conf;
+      };
     };
+    pointerCursor = {
+      gtk.enable = true;
+      package = pkgs.catppuccin-cursors.frappeRosewater;
+      name = "Catppuccin-Frappe-Rosewater-Cursors";
+    };
+    sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+    };
+    packages = with pkgs; [
+      hyprpaper
+      hyprcursor
+      hyprlock
+      nwg-look
+      pavucontrol
+      playerctl
+      kdePackages.breeze
+      kdePackages.qtwayland
+      qt6Packages.qt6ct
+      # libsForQt5.qt5.qtgraphicaleffects
+      feh
+    ];
   };
-  
+
   xdg = {
     enable = true;
     portal = {
@@ -32,38 +58,11 @@
     };
   };
   
-  home.pointerCursor = {
-    gtk.enable = true;
-    package = pkgs.catppuccin-cursors.frappeRosewater;
-    name = "Catppuccin-Frappe-Rosewater-Cursors";
-  };
-
   qt = {
     enable = true;
     platformTheme = "gtk";
   };
-
-  home.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-  };
-
-  home.packages = with pkgs; [
-    hyprpaper
-    hyprcursor
-    nwg-look
-    pavucontrol
-    playerctl
-    # kdePackages.dolphin
-    kdePackages.breeze
-    kdePackages.qtwayland
-    qt6Packages.qt6ct
-    # libsForQt5.qt5.qtgraphicaleffects
-    # qt6.qtwayland
-    # fluent-icon-theme
-    # kora-icon-theme
-    feh
-  ];
-
+  
   services = 
   {
     network-manager-applet.enable = true;
