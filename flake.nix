7a6fad1b5ded2eac0cd3e22c8794e18cd778f7c8
paveun <a.tresierrajansen@gmail.com>
2024-04-15 21:47:36 -0400

    cleanup

--- flake.nix
+++ flake.nix
@@ -14,65 +14,63 @@
     nix-flatpak.url = "github:gmodena/nix-flatpak";
     catppuccin.url = "github:catppuccin/nix";
   };
-
-  outputs = inputs @ { self, nixpkgs, home-manager, nix-flatpak, nixvim, catppuccin, ... }:
+  outputs = { self, nixpkgs, home-manager, nix-flatpak, nixvim, catppuccin, ... } @ inputs:
   let
-    system = "x86_64-linux";
-    # pkgs = import nixpkgs {
-    #   inherit system;
-    #   config.allowUnfree = true;
-    # };
-    lib = nixpkgs.lib;
+    inherit (self) outputs;
+    systems = [
+      "aarch64-linux"
+      "x86_64-linux"
+    ];
     user = "paveun";
+    forAllSystems = nixpkgs.lib.genAttrs systems;
   in 
   {
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
     nixosConfigurations = {
-      # import ./home/hosts {
-      #   inherit (nixpkgs) lib;
-      #   inherit inputs user system home-manager nix-flatpak nixvim catppuccin;
-
-      laptop = lib.nixosSystem {
-        inherit system;
-        specialArgs = { inherit user inputs; };
+      laptop = nixpkgs.lib.nixosSystem {
+        # inherit system;
+        specialArgs = { inherit user inputs outputs; };
         modules = [
           ./hosts/laptop
           catppuccin.nixosModules.catppuccin
+          nix-flatpak.nixosModules.nix-flatpak
           home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = {inherit user; };
-            home-manager.users.${user} = {
-              imports = [
-                ./hosts/laptop/home.nix
-                catppuccin.homeManagerModules.catppuccin
-                nixvim.homeManagerModules.nixvim
-              ];
+            home-manager = { 
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = {inherit user inputs outputs; };
+              users.${user} = {
+                imports = [
+                  ./hosts/laptop/home.nix
+                  catppuccin.homeManagerModules.catppuccin
+                  nixvim.homeManagerModules.nixvim
+                ];
+              };
             };
           }
-          nix-flatpak.nixosModules.nix-flatpak
-          # nixvim.nixosModules.nixvim
         ];
       };
-      fishtank = lib.nixosSystem {
-        inherit system;
-        specialArgs = { inherit user inputs; };
+      fishtank = nixpkgs.lib.nixosSystem {
+        # inherit system;
+        specialArgs = { inherit user inputs outputs; };
         modules = [
           ./hosts/fishtank
           catppuccin.nixosModules.catppuccin
+          nix-flatpak.nixosModules.nix-flatpak
           home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = {inherit user; };
-            home-manager.users.${user} = {
-              imports = [
-                ./hosts/fishtank/home.nix
-                 catppuccin.homeManagerModules.catppuccin
-                 nixvim.homeManagerModules.nixvim
-              ];
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = {inherit user inputs outputs; };
+              users.${user} = {
+                imports = [
+                  ./hosts/fishtank/home.nix
+                  catppuccin.homeManagerModules.catppuccin
+                  nixvim.homeManagerModules.nixvim
+                ];
+              };
             };
           }
-          nix-flatpak.nixosModules.nix-flatpak
-          # nixvim.nixosModules.nixvim
         ];
       };
     };
