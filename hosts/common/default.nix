6d6d69c95d5ca95b925242e90dbc8bbec8c0d729
paveun <a.tresierrajansen@gmail.com>
2024-04-17 09:38:46 -0400

    add devenv binary cache prompt

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -26,6 +26,16 @@
     '';
     # Garbage collection
     settings.auto-optimise-store = true;
+    settings = {
+      extra-substituters = [
+        "https://nixpkgs-python.cachix.org"
+        "https://devenv.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      ];
+    };
     gc = {
       automatic = true;
       dates = "weekly";
