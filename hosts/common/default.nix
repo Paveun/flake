4c0ad2f6316b482c65b4fa2ce1edc557d99baf44
paveun <a.tresierrajansen@gmail.com>
2024-04-17 21:35:09 -0400

    disable nix.gh.automatic

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -36,11 +36,11 @@
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       ];
     };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-    };
+    # gc = {
+    #   automatic = true;
+    #   dates = "weekly";
+    #   options = "--delete-older-than 7d";
+    # };
   };
 
   nixpkgs.config.allowUnfree = true;
