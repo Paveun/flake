27296a66c0eaddca95bf69b429143421aed846d3
paveun <a.tresierrajansen@gmail.com>
2024-04-18 18:45:40 -0400

    clean up

--- flake.nix
+++ flake.nix
@@ -41,58 +41,30 @@
     ];
     user = "paveun";
     forAllSystems = nixpkgs.lib.genAttrs systems;
-    # nixos-modules = [
-    #   catppuccin.nixosModules.catppuccin
-    #   nix-flatpak.nixosModules.nix-flatpak
-    #   home-manager.nixosModules.home-manager
-    #   {
-    #     home-manager = {
-    #       useGlobalPkgs = true;
-    #       useUserPackages = true;
-    #       extraSpecialArgs = {inherit user inputs outputs;};
-    #     };
-    #   }
-    # ];
-    # home-modules = [
-    #   catppuccin.homeManagerModules.catppuccin
-    #   nixvim.homeManagerModules.nixvim
-    # ];
   in {
     formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
     nixosConfigurations = {
       laptop = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit user inputs outputs;};
-        modules =
-          # nixos-modules
-          # ++ [
-          [
-            ./hosts/laptop
-            {
-              home-manager.users.${user}.imports =
-                # home-modules
-                # ++ [
-                [
-                  ./hosts/laptop/home.nix
-                ];
-            }
-          ];
+        modules = [
+          ./hosts/laptop
+          {
+            home-manager.users.${user}.imports = [
+              ./hosts/laptop/home.nix
+            ];
+          }
+        ];
       };
       fishtank = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit user inputs outputs;};
-        modules =
-          # nixos-modules
-          # ++ [
-          [
-            ./hosts/fishtank
-            {
-              home-manager.users.${user}.imports =
-                # home-modules
-                # ++ [
-                [
-                  ./hosts/fishtank/home.nix
-                ];
-            }
-          ];
+        modules = [
+          ./hosts/fishtank
+          {
+            home-manager.users.${user}.imports = [
+              ./hosts/fishtank/home.nix
+            ];
+          }
+        ];
       };
     };
   };
