6730f855a662c675c22e695c0fa8157f5ab2731e
paveun <a.tresierrajansen@gmail.com>
2024-04-28 04:47:50 -0400

    remove nixConfig substituters

--- flake.nix
+++ flake.nix
@@ -1,14 +1,6 @@
 {
   description = "A flake of Paveun";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-gaming.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-    ];
-  };
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
