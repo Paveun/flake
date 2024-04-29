e89f9a9fefc20fdb5894546452691eae0b961cd2
paveun <a.tresierrajansen@gmail.com>
2024-04-29 07:03:00 -0400

    split sddm config for hyprland

--- nixos/wm/hyprland/default.nix
+++ nixos/wm/hyprland/default.nix
@@ -5,6 +5,9 @@
   lib,
   ...
 }: {
+  imports = [
+    ./sddm.nix
+  ];
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
@@ -20,32 +23,7 @@
         variant = "";
       };
     };
-    displayManager = {
-      sddm = {
-        enable = true;
-        wayland.enable = true;
-        theme = "catppuccin-sddm-corners";
-        # theme = "${(pkgs.fetchFromGitHub {
-        #   owner = "rototrash";
-        #   repo = "tokyo-night-sddm";
-        #   rev = "320c8e74ade1e94f640708eee0b9a75a395697c6";
-        #   sha256 = "JRVVzyefqR2L3UrEK2iWyhUKfPMUNUnfRZmwdz05wL0=";
-        # })}";
-      };
-      defaultSession = "hyprland";
-    };
-    gnome.gnome-keyring.enable = true;
   };
-
-  security.pam.services = {
-    sddm.enableGnomeKeyring = true;
-    hyprlock = {};
-  };
-
-  # security.pam.services = {
-  #   sddm.enableKwallet = true;
-  #   swaylock = {};
-  # };
 
   programs.thunar = {
     enable = true;
@@ -53,13 +31,5 @@
       thunar-archive-plugin
       thunar-volman
     ];
-  };
-
-  environment = {
-    systemPackages = with pkgs; [
-      libsForQt5.qt5.qtgraphicaleffects
-      catppuccin-sddm-corners
-    ];
-    # sessionVariables.NIXOS_OZONE_WL = "1";
   };
 }
---
+++ nixos/wm/hyprland/sddm.nix
@@ -0,0 +1,35 @@
+{
+  pkgs,
+  ...
+}: {
+  services = {
+    displayManager = {
+      sddm = {
+        enable = true;
+        wayland.enable = true;
+        theme = "catppuccin-sddm-corners";
+        # theme = "${(pkgs.fetchFromGitHub {
+        #   owner = "rototrash";
+        #   repo = "tokyo-night-sddm";
+        #   rev = "320c8e74ade1e94f640708eee0b9a75a395697c6";
+        #   sha256 = "JRVVzyefqR2L3UrEK2iWyhUKfPMUNUnfRZmwdz05wL0=";
+        # })}";
+      };
+      defaultSession = "hyprland";
+    };
+    gnome.gnome-keyring.enable = true;
+  };
+
+  security.pam.services = {
+    sddm.enableGnomeKeyring = true;
+    hyprlock = {};
+  };
+
+  environment = {
+    systemPackages = with pkgs; [
+      # libsForQt5.qt5.qtgraphicaleffects
+      catppuccin-sddm-corners
+    ];
+    # sessionVariables.NIXOS_OZONE_WL = "1";
+  };
+}
