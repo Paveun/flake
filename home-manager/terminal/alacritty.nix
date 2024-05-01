a442e898e7d26723ace2c8664652171ca8a17361
paveun <a.tresierrajansen@gmail.com>
2024-05-01 12:17:04 -0400

    switch to color setup with nix-colors

--- home-manager/terminal/alacritty.nix
+++ home-manager/terminal/alacritty.nix
@@ -9,7 +9,7 @@
   programs = {
     alacritty = {
       enable = true;
-      catppuccin.enable = true;
+      # catppuccin.enable = true;
       settings = {
         env = {
           TERM = "alacritty";
@@ -54,6 +54,75 @@
             family = "JetBrains Mono Nerd Font";
             style = "Italic";
           };
+        };
+        colors = {};
+        colors.bright = with config.colorScheme.colors; {
+          black = "#${base04}";
+          blue = "#${base0D}";
+          cyan = "#${base0C}";
+          green = "#${base0B}";
+          magenta = "#${base0E}";
+          red = "#${base08}";
+          white = "#${base07}";
+          yellow = "#${base0A}";
+        };
+        colors.dim = with config.colorScheme.colors; {
+          black = "#${base03}";
+          blue = "#${base0D}";
+          cyan = "#${base0C}";
+          green = "#${base0B}";
+          magenta = "#${base0E}";
+          red = "#${base08}";
+          white = "#${base07}";
+          yellow = "#${base0A}";
+        };
+        colors.normal = with config.colorScheme.colors; {
+          black = "#${base03}";
+          blue = "#${base0D}";
+          cyan = "#${base0C}";
+          green = "#${base0B}";
+          magenta = "#${base0E}";
+          red = "#${base08}";
+          white = "#${base07}";
+          yellow = "#${base0A}";
+        };
+        colors.primary = with config.colorScheme.colors; {
+          background = "#${base00}";
+          bright_foreground = "#${base05}";
+          dim_foreground = "#${base05}";
+          foreground = "#${base05}";
+        };
+        colors.cursor = with config.colorScheme.colors; {
+          cursor ="#${base06}";
+          text = "#${base00}";
+        };
+        colors.footer_bar = with config.colorScheme.colors; {
+          background = "#${base07}";
+          foreground = "#${base00}";
+        };
+        colors.hints.end = with config.colorScheme.colors; {
+          background = "#${base07}";
+          foreground = "#${base00}";
+        };
+        colors.hints.start = with config.colorScheme.colors; {
+          background = "#${base0A}";
+          foreground = "#${base00}";
+        };
+        colors.search.focused_match = with config.colorScheme.colors; {
+          background = "#${base0B}";
+          foreground = "#${base00}";
+        };
+        colors.search.matches = with config.colorScheme.colors; {
+          background = "#${base07}";
+          foreground = "#${base00}";
+        };
+        colors.selection = with config.colorScheme.colors; {
+          background = "#${base06}";
+          text = "#${base00}";
+        };
+        colors.vi_mode_cursor = with config.colorScheme.colors; {
+          background = "#${base07}";
+          foreground = "#${base00}";
         };
       };
     };
