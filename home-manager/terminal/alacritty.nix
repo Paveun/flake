853e8c124d1cf32c1f147d04362ca513c3d815d3
paveun <a.tresierrajansen@gmail.com>
2024-05-01 12:41:24 -0400

    cleanup color setup

--- home-manager/terminal/alacritty.nix
+++ home-manager/terminal/alacritty.nix
@@ -55,74 +55,85 @@
             style = "Italic";
           };
         };
-        colors = {};
-        colors.bright = with config.colorScheme.colors; {
-          black = "#${base04}";
-          blue = "#${base0D}";
-          cyan = "#${base0C}";
-          green = "#${base0B}";
-          magenta = "#${base0E}";
-          red = "#${base08}";
-          white = "#${base07}";
-          yellow = "#${base0A}";
-        };
-        colors.dim = with config.colorScheme.colors; {
-          black = "#${base03}";
-          blue = "#${base0D}";
-          cyan = "#${base0C}";
-          green = "#${base0B}";
-          magenta = "#${base0E}";
-          red = "#${base08}";
-          white = "#${base07}";
-          yellow = "#${base0A}";
-        };
-        colors.normal = with config.colorScheme.colors; {
-          black = "#${base03}";
-          blue = "#${base0D}";
-          cyan = "#${base0C}";
-          green = "#${base0B}";
-          magenta = "#${base0E}";
-          red = "#${base08}";
-          white = "#${base07}";
-          yellow = "#${base0A}";
-        };
-        colors.primary = with config.colorScheme.colors; {
-          background = "#${base00}";
-          bright_foreground = "#${base05}";
-          dim_foreground = "#${base05}";
-          foreground = "#${base05}";
-        };
-        colors.cursor = with config.colorScheme.colors; {
-          cursor ="#${base06}";
-          text = "#${base00}";
-        };
-        colors.footer_bar = with config.colorScheme.colors; {
-          background = "#${base07}";
-          foreground = "#${base00}";
-        };
-        colors.hints.end = with config.colorScheme.colors; {
-          background = "#${base07}";
-          foreground = "#${base00}";
-        };
-        colors.hints.start = with config.colorScheme.colors; {
-          background = "#${base0A}";
-          foreground = "#${base00}";
-        };
-        colors.search.focused_match = with config.colorScheme.colors; {
-          background = "#${base0B}";
-          foreground = "#${base00}";
-        };
-        colors.search.matches = with config.colorScheme.colors; {
-          background = "#${base07}";
-          foreground = "#${base00}";
-        };
-        colors.selection = with config.colorScheme.colors; {
-          background = "#${base06}";
-          text = "#${base00}";
-        };
-        colors.vi_mode_cursor = with config.colorScheme.colors; {
-          background = "#${base07}";
-          foreground = "#${base00}";
+        colors = with config.colorScheme.colors; {
+          bright = {
+            black = "#${base04}";
+            blue = "#${base0D}";
+            cyan = "#${base0C}";
+            green = "#${base0B}";
+            magenta = "#${base0E}";
+            red = "#${base08}";
+            white = "#${base07}";
+            yellow = "#${base0A}";
+          };
+          dim = {
+            black = "#${base03}";
+            blue = "#${base0D}";
+            cyan = "#${base0C}";
+            green = "#${base0B}";
+            magenta = "#${base0E}";
+            red = "#${base08}";
+            white = "#${base07}";
+            yellow = "#${base0A}";
+          };
+          normal = {
+            black = "#${base03}";
+            blue = "#${base0D}";
+            cyan = "#${base0C}";
+            green = "#${base0B}";
+            magenta = "#${base0E}";
+            red = "#${base08}";
+            white = "#${base07}";
+            yellow = "#${base0A}";
+          };
+          primary = {
+            background = "#${base00}";
+            bright_foreground = "#${base05}";
+            dim_foreground = "#${base05}";
+            foreground = "#${base05}";
+          };
+          cursor = {
+            cursor = "#${base06}";
+            text = "#${base00}";
+          };
+          footer_bar = {
+            background = "#${base07}";
+            foreground = "#${base00}";
+          };
+          hints.end = {
+            background = "#${base07}";
+            foreground = "#${base00}";
+          };
+          hints.start = {
+            background = "#${base0A}";
+            foreground = "#${base00}";
+          };
+          search.focused_match = {
+            background = "#${base0B}";
+            foreground = "#${base00}";
+          };
+          search.matches = {
+            background = "#${base07}";
+            foreground = "#${base00}";
+          };
+          selection = {
+            background = "#${base06}";
+            text = "#${base00}";
+          };
+          vi_mode_cursor = {
+            background = "#${base07}";
+            foreground = "#${base00}";
+          };
+          indexed_colors = [
+            {
+              color = "#${base09}";
+              index = 16;
+            }
+            {
+              color = "#${base06}";
+              index = 17;
+            }
+          ];
         };
       };
     };
