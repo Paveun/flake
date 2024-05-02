0401baf101a8d43e68410a1cfa4da7281454dc69
paveun <a.tresierrajansen@gmail.com>
2024-05-02 17:29:38 -0400

    use sops for laptop ssh keys

--- home-manager/paveun/laptop.nix
+++ home-manager/paveun/laptop.nix
@@ -14,6 +14,16 @@
     homeDirectory = "/home/${user}";
     stateVersion = "23.11";
   };
+  sops.secrets = {
+    "private_keys/laptop" = {
+      path = "/home/${user}/.ssh/id_laptop";
+    };
+  };
+  home.file = {
+    ".ssh/id_laptop.pub" = {
+      source = ./keys/id_laptop.pub;
+    };
+  };
   home.packages = with pkgs; [
   ];
 }
