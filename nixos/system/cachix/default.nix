847b8939152c9ea67e83ef94d2024e2f0b13a49e
paveun <a.tresierrajansen@gmail.com>
2024-05-04 03:50:48 -0400

    use extra-substituters instead of base

--- nixos/system/cachix/default.nix
+++ nixos/system/cachix/default.nix
@@ -1,15 +1,15 @@
 {
   nix = {
     settings = {
-      substituters = [
-        "https://cache.nixos.org/"
+      extra-substituters = [
+        # "https://cache.nixos.org/"
         "https://nixpkgs-python.cachix.org"
         "https://devenv.cachix.org"
         "https://nix-gaming.cachix.org"
         "https://nyx.chaotic.cx/"
       ];
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      extra-trusted-public-keys = [
+        # "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
