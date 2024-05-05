1362310b5c1ea14f9e6b42cfb7474f613b2cb340
paveun <a.tresierrajansen@gmail.com>
2024-05-05 15:01:33 -0400

    fix missing services wrapper

--- home-manager/wm/hyprland/dunst.nix
+++ home-manager/wm/hyprland/dunst.nix
@@ -1,23 +1,25 @@
 {config, ...}:{
-  dunst = {
-    enable = true;
-    settings = with config.colorScheme.colors; {
-      global = {
-        frame_color = "#${base07}";
-        separator_color = "frame";
-      };
-      urgency_low = {
-        background = "#${base00}";
-        foreground = "#${base05}";
-      };
-      urgency_normal = {
-        background = "#${base00}";
-        foreground = "#${base05}";
-      };
-      urgency_critical = {
-        background = "#${base00}";
-        foreground = "#${base05}";
-        frame_color = "#${base09}";
+  services = {
+    dunst = {
+      enable = true;
+      settings = with config.colorScheme.colors; {
+        global = {
+          frame_color = "#${base07}";
+          separator_color = "frame";
+        };
+        urgency_low = {
+          background = "#${base00}";
+          foreground = "#${base05}";
+        };
+        urgency_normal = {
+          background = "#${base00}";
+          foreground = "#${base05}";
+        };
+        urgency_critical = {
+          background = "#${base00}";
+          foreground = "#${base05}";
+          frame_color = "#${base09}";
+        };
       };
     };
   };
