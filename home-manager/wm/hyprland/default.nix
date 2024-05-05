50d1ec3da457709291919f2cfcbedd496a576e90
paveun <a.tresierrajansen@gmail.com>
2024-05-05 14:59:34 -0400

    split dunst config

--- home-manager/wm/hyprland/default.nix
+++ home-manager/wm/hyprland/default.nix
@@ -9,6 +9,7 @@
     ./wofi
     ./waybar
     ./hyprland.nix
+    ./dunst.nix
     ../../themes
   ];
 
@@ -80,28 +81,6 @@
       enable = true;
       automount = true;
       tray = "never";
-    };
-    dunst = {
-      enable = true;
-      settings = with config.colorScheme.colors; {
-        global = {
-          frame_color = "#${base07}";
-          separator_color = "frame";
-        };
-        urgency_low = {
-          background = "#${base00}";
-          foreground = "#${base05}";
-        };
-        urgency_normal = {
-          background = "#${base00}";
-          foreground = "#${base05}";
-        };
-        urgency_critical = {
-          background = "#${base00}";
-          foreground = "#${base05}";
-          frame_color = "#${base09}";
-        };
-      };
     };
   };
 
