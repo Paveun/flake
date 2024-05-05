11d3c3f95dabc005315a3be090f5aa4ce5a1f84c
paveun <a.tresierrajansen@gmail.com>
2024-05-05 14:35:54 -0400

    specify which nerdfonts to install

--- nixos/system/fonts/default.nix
+++ nixos/system/fonts/default.nix
@@ -4,7 +4,8 @@
     packages = with pkgs; [
       roboto
       jetbrains-mono
-      nerdfonts
+      # nerdfonts
+      (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" ]; })
       noto-fonts
       noto-fonts-emoji
       noto-fonts-cjk
