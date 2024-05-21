0592faa347e2a14a95dd692061d48f500548d845
Paveun <a.tresierrajansen@gmail.com>
2024-05-21 14:05:23 -0400

    use nvidia beta driver

--- nixos/system/nvidia/default.nix
+++ nixos/system/nvidia/default.nix
@@ -1,4 +1,4 @@
-{config, ...}: {
+{config, lib, ...}: {
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware.opengl = {
@@ -13,6 +13,14 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    # package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "555.42.02";
+      sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
+      sha256_aarch64 = lib.fakeSha256;
+      openSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+      settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+      persistencedSha256 = lib.fakeSha256;
+    };
   };
 }
