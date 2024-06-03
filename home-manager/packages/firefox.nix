2ff8d159ac22586e4481813021605952b4f35203
Paveun <a.tresierrajansen@gmail.com>
2024-06-03 11:36:38 -0400

    update firefox config

--- home-manager/packages/firefox.nix
+++ home-manager/packages/firefox.nix
@@ -11,14 +11,34 @@
         name = "Paveun";
         isDefault = true;
         settings = {
-          "browser.startup.homepage" = "https://www.google.com";
-          "browser.search.defaltenginename" = "Google";
-          "browser.search.order.1" = "Google";
+          "browser.startup.homepage" = "https://kagi.com/";
+          "browser.search.defaltenginename" = "Kagi";
+          "browser.search.order.1" = "Kagi";
+          # privacy
+          "privacy.donottrackheader.enabled" = true;
+          "privacy.trackingprotection.enabled" = true;
+          "privacy.trackingprotection.socialtracking.enabled" = true;
+          "privacy.userContext.enabled" = true;
+          "privacy.userContext.ui.enabled" = true;
+          # Disable telemetry for privacy reasons
+          "toolkit.telemetry.archive.enabled" = false;
+          "toolkit.telemetry.enabled" = false; # enforced by nixos
+          "toolkit.telemetry.server" = "";
+          "toolkit.telemetry.unified" = false;
+          "extensions.webcompat-reporter.enabled" = false; # don't report compability problems to mozilla
+          "datareporting.policy.dataSubmissionEnabled" = false;
+          "datareporting.healthreport.uploadEnabled" = false;
+          "browser.ping-centre.telemetry" = false;
+          "browser.urlbar.eventTelemetry.enabled" = false; # (default)
+          # disable annoying web features
+          "dom.push.enabled" = false;
+          "dom.push.connection.enabled" = false;
+          "dom.battery.enabled" = false;
         };
         search = {
           force = true;
-          default = "Google";
-          order = ["Google" "DuckDuckGo"];
+          default = "Kagi";
+          order = ["Kagi" "Google" "DuckDuckGo"];
           engines = {
             "Nix Packages" = {
               urls = [
@@ -45,6 +65,69 @@
               updateInterval = 24 * 60 * 60 * 1000; # every day
               definedAliases = ["@nw"];
             };
+            "Kagi" = {
+              iconUpdateURL = "https://kagi.com/favicon.ico";
+              updateInterval = 24 * 60 * 60 * 1000;
+              definedAliases = ["@k"];
+              urls = [
+                {
+                  template = "https://kagi.com/search";
+                  params = [
+                    {
+                      name = "q";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+            };
+            "GitHub" = {
+              iconUpdateURL = "https://github.com/favicon.ico";
+              updateInterval = 24 * 60 * 60 * 1000;
+              definedAliases = ["@gh"];
+              urls = [
+                {
+                  template = "https://github.com/search";
+                  params = [
+                    {
+                      name = "q";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+            };
+            "Home Manager" = {
+              icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = ["@hm"];
+              url = [
+                {
+                  template = "https://mipmip.github.io/home-manager-option-search/";
+                  params = [
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+            };
+            "YouTube" = {
+              iconUpdateURL = "https://youtube.com/favicon.ico";
+              updateInterval = 24 * 60 * 60 * 1000;
+              definedAliases = ["@yt"];
+              urls = [
+                {
+                  template = "https://www.youtube.com/results";
+                  params = [
+                    {
+                      name = "search_query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+            };
             "Bing".metaData.hidden = true;
             "Google".metaData.alias = "@g";
           };
@@ -55,6 +138,7 @@
           proton-pass
           privacy-badger
           firefox-color
+          kagi-search
         ];
       };
       meta4 = {
