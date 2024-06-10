4e8e781f9279b70d74e192f5c601130969c9213c
Paveun <a.tresierrajansen@gmail.com>
2024-06-10 12:12:47 -0400

    setup extensions

--- home-manager/wm/gnome/default.nix
+++ home-manager/wm/gnome/default.nix
@@ -1,5 +1,20 @@
-{
+{ pkgs, ... }: {
   imports = [
     ../../themes
   ];
+  dconf = {
+    enable = true;
+    settings = {
+      "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = with pkgs.gnomeExtensions; [
+          appindicator.extensionUuid
+          caffeine.extensionUuid
+          system-monitor.extensionUuid
+          user-themes.extensionUuid
+        ];
+      };
+    };
+  };
 }
