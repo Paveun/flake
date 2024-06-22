3ed1021ae0b5371a9567a773be7be40906cc850f
Paveun <a.tresierrajansen@gmail.com>
2024-06-22 02:05:35 -0400

    remove depricated hardware.opengl.driSupport

--- nixos/system/nvidia/default.nix
+++ nixos/system/nvidia/default.nix
@@ -12,8 +12,8 @@
   hardware = {
     opengl = {
       enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
+      # driSupport = true;
+      # driSupport32Bit = true;
     };
     nvidia = {
       modesetting.enable = true;
