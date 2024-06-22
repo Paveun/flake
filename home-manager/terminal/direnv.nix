dbe0bfb46529b22ed00a143952df320361807df2
Paveun <a.tresierrajansen@gmail.com>
2024-06-22 02:00:34 -0400

    enable fish and bash integration

--- home-manager/terminal/direnv.nix
+++ home-manager/terminal/direnv.nix
@@ -2,6 +2,8 @@
   programs.direnv = {
     enable = true;
     enableZshIntegration = true;
+    enableFishIntegration = true;
+    enableBashIntegration = true;
     nix-direnv.enable = true;
   };
 }
