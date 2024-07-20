eaf2bcd5bd17922fdbe10de427b48883b98b0f9c
paveun <a.tresierrajansen@gmail.com>
2024-07-20 06:42:18 -0400

    enable ssh management with sops

--- home-manager/paveun/aion.nix
+++ home-manager/paveun/aion.nix
@@ -15,17 +15,17 @@
     homeDirectory = "/home/${user}";
     stateVersion = "23.11";
   };
-  # sops.secrets = {
-  #   "private_keys/aion" = {
-  #     path = "/home/${user}/.ssh/id_aion";
-  #     mode = "0400";
-  #   };
-  # };
-  # home.file = {
-  #   ".ssh/id_aion.pub" = {
-  #     source = ./keys/id_aion.pub;
-  #   };
-  # };
+  sops.secrets = {
+    "private_keys/aion" = {
+      path = "/home/${user}/.ssh/id_aion";
+      mode = "0400";
+    };
+  };
+  home.file = {
+    ".ssh/id_aion.pub" = {
+      source = ./keys/id_aion.pub;
+    };
+  };
   home.packages = with pkgs; [
     liquidctl
     # razergenie
