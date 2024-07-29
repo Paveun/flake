ca8d716860d4904c5d568665c526ade09c5e41d2
paveun <a.tresierrajansen@gmail.com>
2024-07-29 11:19:11 -0400

    adjust default window size

--- home-manager/terminal/kitty.nix
+++ home-manager/terminal/kitty.nix
@@ -5,8 +5,8 @@
     shellIntegration.enableZshIntegration = true;
     settings = {
       remember_window_size = "no";
-      initial_window_width = "1280";
-      initial_window_height = "800";
+      initial_window_width = "900";
+      initial_window_height = "600";
     };
   };
 }
