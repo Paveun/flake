e674b28ea9cb7c47a69cb102159ec1953f5db5f7
paveun <a.tresierrajansen@gmail.com>
2024-08-03 19:35:25 -0400

    revert to nvidia stable
    beta causes crashing on game launch

--- nixos/system/nvidia/default.nix
+++ nixos/system/nvidia/default.nix
@@ -18,7 +18,7 @@
       powerManagement.finegrained = false;
       open = true;
       nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.beta;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
   };
 }
