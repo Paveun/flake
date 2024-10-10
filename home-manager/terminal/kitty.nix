063181aee62df4f0d63a06450dba3bb80dc13d83
paveun <a.tresierrajansen@gmail.com>
2024-10-10 14:35:29 -0400

    move kitty theming to kitty.nix

--- home-manager/terminal/kitty.nix
+++ home-manager/terminal/kitty.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}:
 {
+  stylix.targets.kitty.enable = false;
   programs.kitty = {
     enable = true;
     shellIntegration.enableZshIntegration = true;
@@ -7,6 +8,15 @@
       remember_window_size = "no";
       initial_window_width = "900";
       initial_window_height = "600";
+      dynamic_background_opacity = true;
+      background_opacity = 0.85;
+      background_blur = 5;
     };
+    font = {
+      package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];};
+      name = "JetbrainsMono Nerd Font Mono";
+      size = 10;
+    };
+    themeFile = "Catppuccin-Mocha";
   };
 }
--- home-manager/themes/default.nix
+++ home-manager/themes/default.nix
@@ -5,13 +5,4 @@
     ./stylix.nix
     ./wallpaper
   ];
-  stylix.targets.kitty.enable = false;
-  programs.kitty = {
-    font = {
-      package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];};
-      name = "JetbrainsMono Nerd Font Mono";
-      size = 10;
-    };
-    themeFile = "Catppuccin-Mocha";
-  };
 }
