4c9b06c34fdb48b12fe01a64dc468bb4db419f3d
paveun <a.tresierrajansen@gmail.com>
2024-10-19 15:21:02 -0400

    enable amdvlk

--- nixos/system/amdgpu/default.nix
+++ nixos/system/amdgpu/default.nix
@@ -8,12 +8,12 @@
     graphics = {
       enable = true;
       enable32Bit = true;
-      # extraPackages = with pkgs; [
-      #   amdvlk
-      # ];
-      # extraPackages32 = with pkgs; [
-      #   driversi686Linux.amdvlk
-      # ];
+      extraPackages = with pkgs; [
+        amdvlk
+      ];
+      extraPackages32 = with pkgs; [
+        driversi686Linux.amdvlk
+      ];
     };
   };
 }
