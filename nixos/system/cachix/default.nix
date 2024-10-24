d6123a413bde584baf9e305cf9f05caa2f2c0791
paveun <a.tresierrajansen@gmail.com>
2024-10-24 11:13:08 -0400

    update cache settings

--- nixos/system/cachix/default.nix
+++ nixos/system/cachix/default.nix
@@ -1,7 +1,7 @@
 {
   nix = {
     settings = {
-      extra-substituters = [
+      substituters = [
         # "https://cache.nixos.org/"
         "https://nixpkgs-python.cachix.org"
         "https://devenv.cachix.org"
@@ -9,7 +9,7 @@
         "https://nyx.chaotic.cx/"
         "https://cosmic.cachix.org/"
       ];
-      extra-trusted-public-keys = [
+      trusted-public-keys = [
         # "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
