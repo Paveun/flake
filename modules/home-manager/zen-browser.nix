df07e410e953273cb51ed437ea13ea1d17bf164e
paveun <a.tresierrajansen@gmail.com>
2024-11-28 11:02:17 -0500

    update zen to 1.0.1-a.20

--- modules/home-manager/zen-browser.nix
+++ modules/home-manager/zen-browser.nix
@@ -4,9 +4,9 @@
   lib,
   ...
 }: let
-  version = "1.0.1-a.19";
+  version = "1.0.1-a.20";
   downloadUrl = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
-  sha256 = "sha256:1g7nq1yfaya97m43vnkjj1nd9g570viy8hj45c523hcyr1z92rjq";
+  sha256 = "sha256:0yv787qz0xh3m5lr12lbj1q6wmcmviqxh8pdws0xpjjzknhigqnk";
 
   runtimeLibs = with pkgs;
     [
