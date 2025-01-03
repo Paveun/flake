4a17cffb7b654804eec3e6ac9d20043fcc732365
paveun <a.tresierrajansen@gmail.com>
2025-01-03 16:30:54 -0500

    add ghostty cachix

--- nixos/system/cachix/default.nix
+++ nixos/system/cachix/default.nix
@@ -2,21 +2,16 @@
   nix = {
     settings = {
       substituters = [
-        # "https://cache.nixos.org/"
         "https://nixpkgs-python.cachix.org"
         "https://devenv.cachix.org"
-        # "https://nix-gaming.cachix.org"
-        # "https://nyx.chaotic.cx/"
         "https://cosmic.cachix.org/"
+        "https://ghostty.cachix.org"
       ];
       trusted-public-keys = [
-        # "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-        # "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-        # "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-        "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8"
         "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+        "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
       ];
     };
   };
