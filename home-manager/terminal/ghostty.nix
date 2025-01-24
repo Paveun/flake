4756eaf2bd2eb324d7d01666ea0257cfb4d1bdba
paveun <a.tresierrajansen@gmail.com>
2025-01-24 15:48:56 -0500

    set font-family to jetbrains-mono

--- home-manager/terminal/ghostty.nix
+++ home-manager/terminal/ghostty.nix
@@ -1,12 +1,16 @@
-{
+{ pkgs, ... }: {
   programs = {
     ghostty = {
       enable = true;
       enableZshIntegration = true;
       settings = {
-        theme = "rose-pine";
+        theme = "rose-pine-moon";
         font-size = 10;
+        font-family = "JetBrainsMono Nerd Font";
       };
     };
   };
+  home.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+  ];
 }
