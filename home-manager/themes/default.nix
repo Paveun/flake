8069efb44244abf5e7f00f4095a41f117fed408d
paveun <a.tresierrajansen@gmail.com>
2025-02-10 12:57:24 -0500

    disable automatic gtk theming

--- home-manager/themes/default.nix
+++ home-manager/themes/default.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   imports = [
-    ./gtk.nix
+    # ./gtk.nix
     ./stylix.nix
     ./wallpaper
   ];
--- home-manager/themes/gtk.nix
+++ home-manager/themes/gtk.nix
@@ -1,14 +1,14 @@
 {pkgs, ...}: {
   gtk = {
     enable = true;
-    iconTheme = {
-      # name = "rose-pine";
-      # package = pkgs.rose-pine-icon-theme;
-      name = "Papirus-Dark";
-      package = pkgs.catppuccin-papirus-folders.override {
-        flavor = "mocha";
-        accent = "rosewater";
-      };
-    };
+    # iconTheme = {
+    #   # name = "rose-pine";
+    #   # package = pkgs.rose-pine-icon-theme;
+    #   name = "Papirus-Dark";
+    #   package = pkgs.catppuccin-papirus-folders.override {
+    #     flavor = "mocha";
+    #     accent = "rosewater";
+    #   };
+    # };
   };
 }
