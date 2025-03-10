8b3985f2221194303de8defd634450a95c0549bf
paveun <a.tresierrajansen@gmail.com>
2025-03-10 11:03:54 -0400

    switch from podman to docker

--- nixos/containers/default.nix
+++ nixos/containers/default.nix
@@ -1,21 +1,30 @@
 {
   configs,
   pkgs,
+  user,
   ...
 }: {
   virtualisation = {
-    podman = {
+    # podman = {
+    #   enable = true;
+    #   # Create a `docker` alias for podman, to use it as a drop-in replacement
+    #   dockerCompat = true;
+    #   # Required for containers under podman-compose to be able to talk to each other.
+    #   defaultNetwork.settings = {
+    #     dns_enabled = true;
+    #   };
+    # };
+    docker = {
       enable = true;
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings = {
-        dns_enabled = true;
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
       };
     };
   };
+  users.users.${user}.extraGroups = [ "docker" ];
   environment.systemPackages = [
-    # pkgs.docker-compose
-    pkgs.podman-compose
+    pkgs.docker-compose
+    # pkgs.podman-compose
   ];
 }
