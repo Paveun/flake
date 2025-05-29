38a563960f0cc041e97549a23868769c2cbe5e1e
paveun <a.tresierrajansen@gmail.com>
2025-05-29 00:08:36 -0400

    switch to zen kernel

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -22,8 +22,8 @@
   #   ./home.nix
   # ];
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  # boot.kernelPackages = pkgs.linuxPackages_zen;
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
   # boot.kernelPackages = pkgs.linuxPackages_cachyos;
   # services.scx = {
   #   enable = true;
